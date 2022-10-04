0184e89aa21bec1acdfdbe1c4f33342308133c24
M-Naaz <mobassera@lirctek.com>
2022-10-04 14:06:51 +0530

    updated

--- src/Website.js
+++ src/Website.js
@@ -14,14 +14,13 @@ class Website extends Component {
         <hr className="line"></hr>
         <img classname="picture2" src={user} />
         <img className="picture3" src={body} />
-        <p>hey guys, What's up! 🥳</p>
-        <p className="content">
-          This UI demonstrates the workflow of the website
-        </p>
+        <div className="content">
+          <p>hey guys, What's up! 🥳</p>
+          <p>This UI demonstrates the workflow of the website</p>
 
-        <p>Let's create something amazing together! 🧑‍🎨</p>
-        <p>to contact me - ertuken@gmail.com</p>
-
+          <p>Let's create something amazing together! 🧑‍🎨</p>
+          <p>to contact me - ertuken@gmail.com</p>
+        </div>
         <img className="picture4" src={footer} />
       </Col>
     );
